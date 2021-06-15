d18cd94f85d31f93c735c29d44a968174f5d6e93
Rob Kuijlenburg <robkuijlenburg91@gmail.com>
2021-06-15 14:02:34 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const productPrice = document.getElementsByClassName('productPrice');
 const productQuantity = document.getElementsByClassName('productQuantity');
 const productTotalCost = document.getElementsByClassName('productTotalCost');
+const totalCost = document.getElementsByClassName('totalCost');
 const qChange = document.getElementsByClassName('quantityChange');
 const add = document.getElementsByClassName('add');
 const sub = document.getElementsByClassName('sub');
@@ -23,30 +24,39 @@ for (i = 0; i < productTotalCost.length; i++) {
 
 for (i = 0; i < sub.length; i++) {
     const qyantityElement = productQuantity[i]
-    sub[i].addEventListener('click', function(){
-       if (parseInt(qyantityElement.innerHTML) > 0) {
-        const count = parseInt(qyantityElement.innerHTML) - 1;
-        qyantityElement.innerHTML = count
-        console.log(count);
-        console.log('-');
-       } else {
-           console.log('Minimal value is 0');
-       }
+    sub[i].addEventListener('click', function () {
+        if (parseInt(qyantityElement.innerHTML) > 0) {
+            const count = parseInt(qyantityElement.innerHTML) - 1;
+            qyantityElement.innerHTML = count;
+            console.log(count);
+            console.log('-');
+        } else {
+            console.log('Minimal value is 0');
+        }
     })
 }
 
 for (i = 0; i < add.length; i++) {
     const qyantityElement = productQuantity[i]
-    add[i].addEventListener('click', function(){
+    add[i].addEventListener('click', function () {
         const count = parseInt(qyantityElement.innerHTML) + 1;
-        qyantityElement.innerHTML = count
+        qyantityElement.innerHTML = count;
         console.log('+');
         console.log(count);
     })
 }
 
 for (i = 0; i < qChange.length; i++) {
-    qChange[i].addEventListener('click', function() {
+    const priceElement = productPrice[i];
+    const qyantityElement = productQuantity[i];
+    const productTotalElement = productTotalCost[i];
+    // const next = productTotalCost[i] + 1;
+    qChange[i].addEventListener('click', function () {
+        const price = parseFloat(priceElement.innerHTML);
+        const quantity = parseInt(qyantityElement.innerHTML)
+        const subTotal =  price * quantity;
+        // const total = subTotal + parseInt(next);
+        productTotalElement.innerHTML = subTotal;
         console.log('aantal producten is gewijzigd');
     })
 }
