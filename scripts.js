e3ea32a63049483ba5e3111c5bc0ec109a509030
Rob Kuijlenburg <robkuijlenburg91@gmail.com>
2021-06-16 17:11:19 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,16 +10,11 @@ const qChange = document.getElementsByClassName('quantityChange');
 const add = document.getElementsByClassName('add');
 const sub = document.getElementsByClassName('sub');
 
-// console.log(productPrice);
-// console.log(productQuantity);
-// console.log(productTotalCost);
 
 for (let i = 0; i < sub.length; i++) {
-    sub[i].addEventListener('click', function () {
-        // now  that we use strict mode we can define the variable inside the event listener
-        // because let makes it block scoped
-        const qyantityElement = productQuantity[i]
+    sub[i].addEventListener('click', function () {  
         if (parseInt(qyantityElement.innerHTML) > 0) {
+            const qyantityElement = productQuantity[i]
             const count = parseInt(qyantityElement.innerHTML) - 1;
             qyantityElement.innerHTML = count;
             console.log(count);
@@ -30,9 +25,9 @@ for (let i = 0; i < sub.length; i++) {
     })
 }
 
-for (let i = 0; i < add.length; i++) {
-    const qyantityElement = productQuantity[i]
+for (let i = 0; i < add.length; i++) {   
     add[i].addEventListener('click', function () {
+        const qyantityElement = productQuantity[i]
         const count = parseInt(qyantityElement.innerHTML) + 1;
         qyantityElement.innerHTML = count;
         console.log('+');
@@ -52,33 +47,16 @@ for (let i = 0; i < qChange.length; i++) {
         const subTotal = price * quantity;
         productTotalElement.innerHTML = Math.round(subTotal * 100) / 100;
         const newArray = [];
-   
-        // TODO :: don't use the same variable for a loop in a loop, cause what's i inside the loop here?
-        for (let i = 0; i < productTotalCost.length; i++) {
-            console.log(productTotalCost[i].innerText);
-            for (let i = 0; i < productTotalCost.length; i++) {
-                const a = productTotalCost[i];
+        for (let c = 0; c < productTotalCost.length; c++) {
+            console.log(productTotalCost[c].innerText);
+            for (let d = 0; d < productTotalCost.length; d++) {
+                const a = productTotalCost[d];
                 const b = parseFloat(a.innerText);
-
-                // TODO :: remove bpush variable, because it's an unused variable
-                let bpush = newArray.push(b);
-                bpush;
-                console.log(newArray);
-                // TODO :: try to use the reduce method to get the total:
-                // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-                let total = 0;
-                console.log(b);
-                for (let i = 0; i < newArray.length; i++) {
-                    total += newArray[i];
-                }
-                totalCost[0].innerHTML =  Math.round(total * 100) / 100;
-                console.log(total);
-               
+                newArray.push(b);
+                const reducer = (accumulator, currentValue) => accumulator + currentValue;
+                totalCost[0].innerHTML =  Math.round(newArray.reduce(reducer) * 100) / 100;  
             }
-        }
-
-        
-        console.log(productTotalElement.innerText);
+        }      
         console.log('aantal producten is gewijzigd');
     })
 }
